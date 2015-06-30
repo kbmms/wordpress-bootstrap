05d9387d09f2217469dc2c66db377880da65d6db
Marcel <roxsublime@gmail.com>
2015-06-30 01:29:23 -0300

    Tema wordpress com bootstrap

---
+++ footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+?>
+
+	<footer>
+
+<?php
+	get_sidebar( 'footer' );
+?>
+
+		<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<?php bloginfo( 'name' ); ?>
+		</a>
+
+		<?php do_action( 'starkers_credits' ); ?>
+
+		<a href="<?php echo esc_url( __('http://wordpress.org/', 'starkers') ); ?>" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'starkers'); ?>" rel="generator">
+			<?php printf( __('Proudly powered by %s.', 'starkers'), 'WordPress' ); ?>
+		</a>
+
+	</footer>
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.11.3.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>js/angular.min.js"></script>
+</body>
+</html>
